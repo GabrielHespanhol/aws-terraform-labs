37ac4268a7c04c5c322bbe74c5dad0a31dffb367
Gabriel Lopes Hespanhol <61161162+GabrielHespanhol@users.noreply.github.com>
2026-04-18 14:56:02 -0300

    chore: adjust key-pair example

--- key-pair.tf
+++ key-pair.tf
@@ -5,7 +5,7 @@
 # Você deve colar o valor de sua chave pública SSH no campo public_key
 resource "aws_key_pair" "linuxtips_key_pair" {
   key_name   = "linuxtips-key-pair"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB0qGO86UtpESUp/1OuUEBtxb0F4p5qA3fHpMDlsH7Lx gabrielhespanhol@jedi"
+  public_key = "chave-ssh-aqui"
 
   tags = {
     Terraform   = "true"
